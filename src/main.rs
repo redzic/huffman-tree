ab6be90874e0d5b1abd89a3ad82e7c8178f8d7c5
Redzic <yusufredzic@gmail.com>
2022-05-29 20:20:29 -0500

    Add in-place heap sort

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,69 @@ pub fn heap_sort(x: &mut [u8]) {
     }
 }
 
+pub fn pop_front_max_heap2(x: &mut [u8]) -> Option<u8> {
+    // TODO handle edge cases where it's not perfectly aligned
+    // also this could be empty...
+
+    let mut idx = 0;
+
+    if x.is_empty() {
+        return None;
+    }
+
+    if x.len() == 1 {
+        return Some(x[0]);
+    }
+
+    let ret = x[0];
+
+    // swap last node and root node
+    let last = x[x.len() - 1];
+    x[0] = last;
+
+    loop {
+        let root_node = x[idx];
+
+        // idx points to root node
+
+        // get child nodes
+        let c1 = x.get(2 * idx + 1);
+        let c2 = x.get(2 * idx + 2);
+
+        match (c1, c2) {
+            (Some(&left), Some(&right)) => {
+                let largest_child_idx = if left >= right { 1 } else { 2 };
+
+                let max = root_node.max(left).max(right);
+                if root_node != max {
+                    // need to swap with largest in that case
+                    // and traverse down the tree
+                    x.swap(idx, 2 * idx + largest_child_idx);
+
+                    // adjust index, go to child node that was larger
+                    idx = 2 * idx + largest_child_idx;
+                } else {
+                    break;
+                }
+            }
+            (Some(&left), None) => {
+                // compare last child node with parent node
+                // since this is a max heap, the child node should not be greater than
+                // the parent
+                if left > root_node {
+                    x.swap(idx, 2 * idx + 1);
+                }
+                break;
+            }
+            // shouldn't happen, means the tree is corrupt
+            (None, Some(_)) => unsafe { std::hint::unreachable_unchecked() },
+            (None, None) => break,
+        }
+    }
+
+    Some(ret)
+}
+
 pub fn pop_front_max_heap(x: &mut Vec<u8>) -> Option<u8> {
     // TODO handle edge cases where it's not perfectly aligned
     // also this could be empty...
@@ -47,7 +110,7 @@ pub fn pop_front_max_heap(x: &mut Vec<u8>) -> Option<u8> {
                 if root_node != max {
                     // need to swap with largest in that case
                     // and traverse down the tree
-                    unsafe { swap(&mut x[idx], &mut x[2 * idx + largest_child_idx]) }
+                    x.swap(idx, 2 * idx + largest_child_idx);
 
                     // adjust index, go to child node that was larger
                     idx = 2 * idx + largest_child_idx;
@@ -60,9 +123,7 @@ pub fn pop_front_max_heap(x: &mut Vec<u8>) -> Option<u8> {
                 // since this is a max heap, the child node should not be greater than
                 // the parent
                 if left > root_node {
-                    unsafe {
-                        swap(&mut x[idx], &mut x[2 * idx + 1]);
-                    }
+                    x.swap(idx, 2 * idx + 1);
                 }
                 break;
             }
@@ -73,6 +134,41 @@ pub fn pop_front_max_heap(x: &mut Vec<u8>) -> Option<u8> {
     }
 
     Some(ret)
+}
+
+pub fn heap_sort2(mut x: &mut [u8]) {
+    // build into max heap
+    build_max_heap2(x);
+
+    while let Some(v) = pop_front_max_heap2(x) {
+        let len = x.len();
+        x[len - 1] = v;
+        x = &mut x[..len - 1];
+    }
+}
+
+// build max heap in-place
+pub fn build_max_heap2(x: &mut [u8]) {
+    for mut idx in 0..x.len() {
+        let c_node = x[idx];
+
+        loop {
+            if idx == 0 {
+                break;
+            }
+            let p_node = x[(idx - 1) / 2];
+            // check if parent node is greater than current node
+            if p_node < c_node {
+                // swap current node with parent node
+                x.swap(idx, (idx - 1) / 2);
+                // set index to parent node
+                idx = (idx - 1) / 2;
+            } else {
+                // else no swapping required
+                break;
+            }
+        }
+    }
 }
 
 pub fn build_max_heap(x: &[u8]) -> Vec<u8> {
@@ -88,10 +184,8 @@ pub fn build_max_heap(x: &[u8]) -> Vec<u8> {
             let p_node = output[(idx - 1) / 2];
             // check if parent node is greater than current node
             if p_node < c_node {
-                unsafe {
-                    // swap current node with parent node
-                    swap(&mut output[idx], &mut output[(idx - 1) / 2]);
-                }
+                // swap current node with parent node
+                output.swap(idx, (idx - 1) / 2);
                 // set index to parent node
                 idx = (idx - 1) / 2;
             } else {
@@ -109,13 +203,14 @@ fn main() {
     // let chars = [1u8, 5, 6, 7, 8, 2, 1, 3, 9, 1];
     let mut chars = [3, 2, 1, 5, 3, 7, 9, 12, 255, 255, 38, 2, 3, 4, 69];
 
-    heap_sort(&mut chars);
+    // heap_sort(&mut chars);
 
-    println!("chars: {chars:?}");
+    // println!("chars: {chars:?}");
 
-    // let mut bt = build_max_heap(&chars);
+    heap_sort2(&mut chars);
+    // build_max_heap2(&mut chars);
 
-    // println!("bt before: {:?}\n", bt);
+    println!("max heap: {:?}\n", chars);
 
     // // correct: 1 5 1 7 8 6 2 3
 
