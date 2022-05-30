83c59cdededd5a01770cfd0c0bce988bc6d087ff
Redzic <yusufredzic@gmail.com>
2022-05-29 20:45:42 -0500

    remove out-of-place heapsort, add correctness test

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,6 @@
-use std::ptr::swap;
+use rand::prelude::*;
 
-pub fn heap_sort(x: &mut [u8]) {
-    let mut max_heap = build_max_heap(x);
-
-    let mut idx = x.len();
-    while let Some(root) = pop_front_max_heap(&mut max_heap) {
-        idx -= 1;
-        x[idx] = root;
-    }
-}
-
-pub fn pop_front_max_heap2(x: &mut [u8]) -> Option<u8> {
-    // TODO handle edge cases where it's not perfectly aligned
-    // also this could be empty...
-
+pub fn pop_front_max_heap(x: &mut [u8]) -> Option<u8> {
     let mut idx = 0;
 
     if x.is_empty() {
@@ -73,82 +60,8 @@ pub fn pop_front_max_heap2(x: &mut [u8]) -> Option<u8> {
     Some(ret)
 }
 
-pub fn pop_front_max_heap(x: &mut Vec<u8>) -> Option<u8> {
-    // TODO handle edge cases where it's not perfectly aligned
-    // also this could be empty...
-
-    let mut idx = 0;
-
-    if x.is_empty() {
-        return None;
-    }
-
-    if x.len() == 1 {
-        return x.pop();
-    }
-
-    let ret = x[0];
-
-    // swap last node and root node
-    let last = x.remove(x.len() - 1);
-    x[0] = last;
-
-    loop {
-        let root_node = x[idx];
-
-        // idx points to root node
-
-        // get child nodes
-        let c1 = x.get(2 * idx + 1);
-        let c2 = x.get(2 * idx + 2);
-
-        match (c1, c2) {
-            (Some(&left), Some(&right)) => {
-                let largest_child_idx = if left >= right { 1 } else { 2 };
-
-                let max = root_node.max(left).max(right);
-                if root_node != max {
-                    // need to swap with largest in that case
-                    // and traverse down the tree
-                    x.swap(idx, 2 * idx + largest_child_idx);
-
-                    // adjust index, go to child node that was larger
-                    idx = 2 * idx + largest_child_idx;
-                } else {
-                    break;
-                }
-            }
-            (Some(&left), None) => {
-                // compare last child node with parent node
-                // since this is a max heap, the child node should not be greater than
-                // the parent
-                if left > root_node {
-                    x.swap(idx, 2 * idx + 1);
-                }
-                break;
-            }
-            // shouldn't happen, means the tree is corrupt
-            (None, Some(_)) => unsafe { std::hint::unreachable_unchecked() },
-            (None, None) => break,
-        }
-    }
-
-    Some(ret)
-}
-
-pub fn heap_sort2(mut x: &mut [u8]) {
-    // build into max heap
-    build_max_heap2(x);
-
-    while let Some(v) = pop_front_max_heap2(x) {
-        let len = x.len();
-        x[len - 1] = v;
-        x = &mut x[..len - 1];
-    }
-}
-
 // build max heap in-place
-pub fn build_max_heap2(x: &mut [u8]) {
+pub fn build_max_heap(x: &mut [u8]) {
     for mut idx in 0..x.len() {
         let c_node = x[idx];
 
@@ -171,51 +84,35 @@ pub fn build_max_heap2(x: &mut [u8]) {
     }
 }
 
-pub fn build_max_heap(x: &[u8]) -> Vec<u8> {
-    let mut output = vec![0u8; x.len()];
+pub fn heap_sort(mut x: &mut [u8]) {
+    // build into max heap
+    build_max_heap(x);
 
-    for (mut idx, &c_node) in x.iter().enumerate() {
-        output[idx] = c_node;
-
-        loop {
-            if idx == 0 {
-                break;
-            }
-            let p_node = output[(idx - 1) / 2];
-            // check if parent node is greater than current node
-            if p_node < c_node {
-                // swap current node with parent node
-                output.swap(idx, (idx - 1) / 2);
-                // set index to parent node
-                idx = (idx - 1) / 2;
-            } else {
-                // else no swapping required
-                break;
-            }
-        }
+    while let Some(v) = pop_front_max_heap(x) {
+        let len = x.len();
+        x[len - 1] = v;
+        x = &mut x[..len - 1];
     }
-
-    output
 }
 
 fn main() {
     // let chars: Vec<u8> = (0..32).collect();
     // let chars = [1u8, 5, 6, 7, 8, 2, 1, 3, 9, 1];
-    let mut chars = [3, 2, 1, 5, 3, 7, 9, 12, 255, 255, 38, 2, 3, 4, 69];
+    let cap = 4096;
 
-    // heap_sort(&mut chars);
+    let mut buf1 = vec![0u8; cap];
+    let mut buf2 = vec![0u8; cap];
 
-    // println!("chars: {chars:?}");
+    let mut rng = rand::thread_rng();
+    loop {
+        let len = rng.gen_range(0..cap);
 
-    heap_sort2(&mut chars);
-    // build_max_heap2(&mut chars);
+        buf1[..len].fill_with(|| rng.gen());
+        buf2[..len].copy_from_slice(&buf1[..len]);
 
-    println!("max heap: {:?}\n", chars);
+        heap_sort(&mut buf1[..len]);
+        buf2[..len].sort();
 
-    // // correct: 1 5 1 7 8 6 2 3
-
-    // while let Some(val) = pop_front_max_heap(&mut bt) {
-    //     println!("popped {val}");
-    //     println!("bt after: {bt:?}\n");
-    // }
+        assert_eq!(buf1[..len], buf2[..len]);
+    }
 }
